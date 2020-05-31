239a0f07ce5e1d9e82df1d10aed0f640cba7f266
rim112 <rim.thaimi@gmail.com>
2020-05-31 23:37:57 +0100

    initial commit rim

--- src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/EvaluationRepository.java
+++ src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/EvaluationRepository.java
@@ -1,13 +1,12 @@
 package com.Tourisme.microTourisme.Model.Services.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.Tourisme.microTourisme.Model.Services.Voyage;
 
 @CrossOrigin("*")
-@RepositoryRestResource
 public interface EvaluationRepository extends JpaRepository<Voyage,Integer>  {
 
 }
--- src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/ParpffRepository.java
+++ src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/ParpffRepository.java
@@ -1,14 +1,14 @@
 package com.Tourisme.microTourisme.Model.Services.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.Tourisme.microTourisme.Model.Services.Parpff;
 
 
 @CrossOrigin("*")
-@RepositoryRestResource
+
 public interface ParpffRepository extends JpaRepository<Parpff,Integer>  {
 
 }
